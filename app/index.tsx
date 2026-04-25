000c97681300b60e9d969a99ad533eaa9127e904
PC <davoudi.kian2007@gmail.com>
2026-04-24 20:39:34 -0400

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl text-primary font-bold">Hello Kian, and Shabir!</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-2xl text-primary font-bold">
+                Hello Kian, and Shabir. I like A48!
+            </Text>
+        </View>
+    );
 }
