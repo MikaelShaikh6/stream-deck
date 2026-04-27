d73041e9e2fb79edfea05dfcfcd89b88eb126549
Mikael Shaikh <mikael2007s@gmail.com>
2026-04-26 20:29:30 -0400

    Added Settings Modal

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,15 @@
 import { Text, View } from "react-native";
+import Modal from 'react-native-modal';
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-colors-background">
-      <Text className="text-2xl text-primary font-bold"></Text>
+      <Text style={{color: "blue"}} className="text-2xl text-primary font-bold"></Text>
+      <Modal isVisible={true}>
+        <View className="flex-1 ">
+          
+        </View>
+      </Modal>
     </View>
+    
   );
 }
