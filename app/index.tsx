2427c527dc1d4b4c0959d49e51da942fd654f21e
PC <davoudi.kian2007@gmail.com>
2026-05-04 13:58:19 -0400

    current version, not fully working but getting there

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import {colors} from "@/constants/colors";
 
 export default function Index() {
+  const main_button_prop = "w-20 h-5 hover:bg-black/10";
+
   const [settingsVisible, setSettingsVisible] = useState(false);
   const [buttonsVisible, setButtonsVisible] = useState(false);
   const [displayVisible, setDisplayVisible] = useState(false);
@@ -29,15 +31,15 @@ export default function Index() {
         <Pressable onPress={toggleSettings} className="bg-black/10 rounded-xl absolute top-5 right-5">
           <Ionicons name="settings" size={32} color="black"/>
           <Modal transparent={true} visible={settingsVisible} onRequestClose={toggleSettings} className="relative flex-1 justify-center items-center">
-            <View className="absolute flex-1 flex-col bg-black/50 rounded-xl px-6 py-4 top-10 right-10 gap-2">
-              <Pressable className="bg-background hover:bg-black" onPress={toggleButtons}>
-                <Text className="color-text">Buttons</Text>
+            <View className="size-30 absolute flex-1 flex-col bg-black/50 rounded-xl px-1 py-1 top-10 right-10 gap-2">
+              <Pressable className={`${main_button_prop}`} onPress={toggleButtons}>
+                <Text className="color-text text-center">Buttons</Text>
               </Pressable>
               <Pressable onPress={toggleAudio}>
-                <Text className="color-text">Audio</Text>
+                <Text className="color-text text-center">Audio</Text>
               </Pressable>
               <Pressable onPress={toggleDisplay}>
-                <Text className="color-text">Display</Text>
+                <Text className="color-text text-center">Display</Text>
               </Pressable>
             </View>
           </Modal>
