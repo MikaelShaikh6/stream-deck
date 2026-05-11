e38657dea00c8e5fb4ee3d60a311569612fb69f8
PC <davoudi.kian2007@gmail.com>
2026-05-11 19:40:13 -0400

    Current Version

--- app/index.tsx
+++ app/index.tsx
@@ -85,9 +85,23 @@ export default function Index() {
             <GeneralButton id={"skip backward"} func={skipBackward}>
               <Text className={"select-none"}>skip backwards</Text>
             </GeneralButton>
-            <View className="w-[308px] h-[96px] flex-1 justify-center items-center rounded-xl bg-green-100">
+            <View className="w-[308px] h-[96px] flex-1 justify-center items-center rounded-xl bg-accent">
+              <Text className="mb-4">Volume Control</Text>
               <Slider
                 style={{width: '90%', height: 5,}}
+                minimumValue={0}
+                maximumValue={1}
+                value={volume}
+                onValueChange={(val) => setVolume(val)}
+                minimumTrackTintColor={"grey"}
+                maximumTrackTintColor={"purple"}
+                thumbTintColor={"blue"}
+              />
+            </View>
+
+            <View className="w-[154px] h-[192px] flex-1 justify-center items-center rounded-xl bg-accent">
+              <Slider
+                style={{width: '90%', height: 5, transform: [{rotate: '-90deg'}]}}
                 minimumValue={0}
                 maximumValue={1}
                 value={volume}
